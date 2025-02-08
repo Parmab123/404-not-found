8aa464cd372a2a66cb8987bda323aba761287603
Parmab123 <161801688+Parmab123@users.noreply.github.com>
2025-02-09 02:49:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'], 
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    darkMode: 'class', // or 'media'
+    content: ['./src/**/*.{html,js}'], 
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
